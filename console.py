0b937b797464ac8736810e83f88ab426dde37ced
= <=>
2022-11-25 11:35:15 +0100

    Updates an instance based on ID with a dict

--- console.py
+++ console.py
@@ -32,8 +32,11 @@ CLASSES = [
 parser = argparse.ArgumentParser()
 args = str(parser.parse_args())
 
+# ============== Helper Functions ===============
 
 # Parses input string and stores it in arg vector
+
+
 def parser(args):
     """Parses arguments parsed to our HBNBCommands
     Returns an array of strings
@@ -42,12 +45,28 @@ def parser(args):
     # The regex returns a list of all space-demarcated substrings
     return re.findall(r"(\b[^\s]+\b)", args)
 
+# Helper function to verify a given id
+
+
+def verify_id(id, argv):
+    """Verifies if given id exists and returns True when it does
+    Args:
+        id(str): id to be verified
+        argv(list): list of commands passed to cmd
+    """
+    all_dict = storage.all()
+    key = f"{argv[0]}.{argv[1]}"
+    if all_dict.get(key):
+        return True
+    return False
+
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for AirBnB project"""
-    
+
     prompt = "(hbnb) "
-    
+
+    # Overides onecmd
     def precmd(self, line):
         """Overwrites the onecmd. This helps us to rearrange
         the arguments in the format of class.cmd() to fit into
@@ -60,22 +79,31 @@ class HBNBCommand(cmd.Cmd):
         # checks for arguments in the format of class.cmd()
         if "." in line:
             argv = line.split('.')
-            
-            # Regex returns the command without the ending ()
-            command = re.findall(r"^[a-z]+", argv[1])[0]
-
-            if command in cmd_list:
+            # Regex returns the command (comd) without the ending ()
+            comd = re.findall(r"^[a-z]+", argv[1])[0]
+            if comd in cmd_list:
                 # Gets the id passed to commands requiring id
                 id = re.findall(r"\((.*)\)", argv[1])[0]
-                if command == "update":
-                    args = id.split(' ')
-                    line = f"{command} {argv[0]} {args[0]} {args[1]} {args[2]}"
-                    print(line)
+                if comd == "update":
+                    # av is a list of commands passed to update method
+                    av = id.split(' ')
+                    if len(av) == 3:
+                        # Checks value parameter is passed as a dict
+                        if type(av[2]) is dict:
+                            for k, v in av[2].items():
+                                av[1] = k
+                                av[2] = v
+                            line = f'{comd} {av[0]} {av[0]} {av[1]} {av[2]}'
+                        line = f'{comd} {argv[0]} {av[0]} {av[1]} {av[2]}'
+                    elif len(av) == 2:
+                        line = f"{comd} {argv[0]} {av[0]} {av[1]}"
+                    elif len(av) == 1:
+                        line = f"{comd} {argv[0]} {av[0]}"
                 else:
                     # Reconstructs the line to fit cmd class pattern
-                    line = f"{command} {argv[0]} {id}"
+                    line = f"{comd} {argv[0]} {id}"
             else:
-                line = f"{command} {argv[0]}"
+                line = f"{comd} {argv[0]}"
 
         # Returns the line as an argument to one cmd
         return cmd.Cmd.precmd(self, line)
@@ -101,7 +129,7 @@ class HBNBCommand(cmd.Cmd):
         Saves it to the JSON file and prints the id
         """
         # argv is a vector containing parsed input string
-        # Input string involves only args passed to function 
+        # Input string involves only args passed to function
         argv = parser(args)
 
         if len(argv) == 0:
@@ -161,21 +189,23 @@ class HBNBCommand(cmd.Cmd):
         argv = parser(args)
 
         all_dicts = storage.all()
-        all_objects = [] 
-        class_objects = [] # contains obj when class name is called
-
+        all_objects = []
+        class_objects = []  # contains obj when class name is called
+        # Stores str(obj) in list if class name is valid
         for k, v in all_dicts.items():
             all_objects.append(str(v))
             if len(argv) != 0:
                 cls = v.__class__.__name__
                 if argv[0] == cls:
                     class_objects.append(str(v))
+        # No class given
         if len(argv) == 0:
             print(all_objects)
+        # Class name given and is a valid class
         elif len(argv) == 1:
             if argv[0] in CLASSES:
                 print(class_objects)
-            
+        # Class name not a valid class
         else:
             print("** class doesn't exist **")
 
@@ -183,6 +213,7 @@ class HBNBCommand(cmd.Cmd):
         """Updates an instance based on the class and id by adding attribute
         """
         argv = parser(args)
+        flag = 0
 
         if len(argv) == 0:
             print("** class name missing **")
@@ -191,30 +222,35 @@ class HBNBCommand(cmd.Cmd):
         elif len(argv) == 1:
             print("** instance id missing **")
         elif len(argv) == 2:
-            all_dict = storage.all()
-            key = f"{argv[0]}.{argv[1]}"
-            if all_dict.get(key) is None:
+            if verify_id(argv[1], argv) is False:
                 print("** no instance found **")
             else:
                 print("** attribute name missing **")
+
         elif len(argv) == 3:
-            print("** value missing **")
-        else:
-            all_dict = storage.all()
-            key = f"{argv[0]}.{argv[1]}"
-            obj = all_dict.get(key)
-
-            # if attribute name already exist, verify its type
-            # and cast new value to the existing type
-            if argv[2] in type(obj).__dict__:
-                attr_type = type(obj.__class__.__dict__[argv[2]])
-                setattr(obj, argv[2], attr_type(argv[3]))
+            if verify_id(argv[1], argv) is False:
+                print("** no instance found **")
             else:
-                # if attribute does not exist, just assign to value
-                setattr(obj, argv[2], argv[3])
+                print("** value missing **")
+        else:
+            if verify_id(argv[1], argv) is False:
+                print("** no instance found **")
+            else:
+                all_dict = storage.all()
+                key = f"{argv[0]}.{argv[1]}"
+                obj = all_dict.get(key)
 
-            # save the updated objects
-            storage.save()
+                # if attribute name already exist, verify its type
+                # and cast new value to the existing type
+                if argv[2] in type(obj).__dict__:
+                    attr_type = type(obj.__class__.__dict__[argv[2]])
+                    setattr(obj, argv[2], attr_type(argv[3]))
+                else:
+                    # if attribute does not exist, just assign to value
+                    setattr(obj, argv[2], argv[3])
+
+                # save the updated objects
+                storage.save()
 
     def do_count(self, args):
         """Retrives the count of instances of a class
--- models/base_model.py
+++ models/base_model.py
@@ -23,6 +23,8 @@ class BaseModel:
                 elif key == "updated_at":
                     value = datetime.fromisoformat(value)
                     self.updated_at = value
+                else:
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
